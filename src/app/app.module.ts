481123f75d5b18ab14480f250727915f5b551fef
Fabian <flagosp21@gmail.com>
2023-11-07 15:25:05 -0300

    IntegracionFirebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"tiendaelectronica-86ae2","appId":"1:1094009642676:web:b2d39bc443de6a5d9ec1e1","storageBucket":"tiendaelectronica-86ae2.appspot.com","apiKey":"AIzaSyB44aFZc1vrjUf3GnIE0HratUybw9WNYTU","authDomain":"tiendaelectronica-86ae2.firebaseapp.com","messagingSenderId":"1094009642676","measurementId":"G-CW0FH0JTE4"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
