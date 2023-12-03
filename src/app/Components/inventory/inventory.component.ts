2e9b975355fe57d042dd8553e039a88d24038e68
Fabian <flagosp21@gmail.com>
2023-12-03 14:23:43 -0300

    Vizualizar y Modificar

--- src/app/Components/inventory/inventory.component.ts
+++ src/app/Components/inventory/inventory.component.ts
@@ -1,36 +1,115 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+// inventory.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CollectionService } from 'src/app/Services/collection.service';
+import { Inventory } from 'src/app/Interfaces/interfaces';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.css']
+  styleUrls: ['./inventory.component.css'],
 })
-export class InventoryComponent {
-  
+export class InventoryComponent implements OnInit {
   inventory: FormGroup;
+  inventoryData: Inventory[] = [];
+  selectedProduct: Inventory | null = null;
+  editForm: FormGroup;
 
-  constructor(
-    private collectionService: CollectionService 
-  ) {
+  constructor(private collectionService: CollectionService) {
     this.inventory = new FormGroup({
-      nameProduct: new FormControl(),
-      description: new FormControl(),
-      unitPrice: new FormControl(),
-      category: new FormControl(), order: new FormControl(),
-      stock: new FormControl(),
-      stockMinimun: new FormControl(),
+      nameProduct: new FormControl('', Validators.required),
+      description: new FormControl(''),
+      unitPrice: new FormControl(0, Validators.min(0)),
+      category: new FormControl(''),
+      order: new FormControl(0, Validators.min(0)),
+      stock: new FormControl(0, Validators.min(0)),
+      stockMinimun: new FormControl(0, Validators.min(0)),
     });
 
+    this.editForm = new FormGroup({
+      nameProduct: new FormControl('', Validators.required),
+      description: new FormControl(''),
+      unitPrice: new FormControl(0, Validators.min(0)),
+      category: new FormControl(''),
+      order: new FormControl(0, Validators.min(0)),
+      stock: new FormControl(0, Validators.min(0)),
+      stockMinimun: new FormControl(0, Validators.min(0)),
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loadInventoryData();
+  }
+
+  async loadInventoryData() {
+    this.inventoryData = await this.collectionService.getAllInventory();
   }
 
   async onSubmit() {
-    console.log(this.inventory.value)
-    const response = await this.collectionService.addInventory(this.inventory.value);
-    console.log(response);
+    if (this.inventory.valid) {
+      const newProduct: Inventory = this.inventory.value;
+
+      try {
+        await this.collectionService.addInventory(newProduct);
+        console.log('Producto agregado exitosamente');
+        this.loadInventoryData();
+        this.inventory.reset();
+      } catch (error) {
+        console.error('Error al agregar el producto:', error);
+      }
+    }
+  }
+
+  async editProduct(product: Inventory) {
+    // Obtener la UID del documento
+    const uid = product.idProduct;
+
+    if (uid) {
+      // Obtener el documento completo usando la UID
+      const updatedProduct = await this.collectionService.getInventoryByUid(uid);
+
+      if (updatedProduct) {
+        this.selectedProduct = updatedProduct;
+        this.editForm.setValue({
+          nameProduct: updatedProduct.nameProduct,
+          description: updatedProduct.description,
+          unitPrice: updatedProduct.unitPrice,
+          category: updatedProduct.category,
+          order: updatedProduct.order,
+          stock: updatedProduct.stock,
+          stockMinimun: updatedProduct.stockMinimun,
+        });
+      } else {
+        console.error('Error: No se encontró el producto con la UID proporcionada');
+      }
+    } else {
+      console.error('Error: idProduct es nulo o indefinido');
+    }
+  }
+
+  async saveEdit() {
+    if (this.selectedProduct) {
+      const updatedProduct: Inventory = {
+        ...this.selectedProduct,
+        ...this.editForm.value,
+      };
+
+      if (this.selectedProduct.idProduct !== undefined) {
+        await this.collectionService.updateInventory(
+          this.selectedProduct.idProduct,
+          updatedProduct
+        );
+        await this.loadInventoryData();
+        this.clearEditForm();
+      } else {
+        console.error('Error: idProduct es undefined');
+      }
+    }
+  }
+
+  clearEditForm() {
+    this.selectedProduct = null;
+    this.editForm.reset();
   }
 }
--- src/app/Services/collection.service.ts
+++ src/app/Services/collection.service.ts
@@ -1,20 +1,86 @@
+// collection.service.ts
+
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc} from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  getDoc,
+  DocumentData,
+  where,
+  query,
+} from '@angular/fire/firestore';
 import { Inventory, Sales } from '../Interfaces/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CollectionService {
 
   constructor(private firestore: Firestore) { }
 
-  addSales(sales: Sales){
-    const collectionRef = collection(this.firestore, 'sales')
-    return addDoc(collectionRef, sales);
+  async addInventory(inventory: Inventory): Promise<void> {
+    const collectionRef = collection(this.firestore, 'inventory');
+
+    // Agregar el documento sin proporcionar el idProduct (Firebase generará uno automáticamente)
+    const docRef = await addDoc(collectionRef, { ...inventory });
+
+    // Obtener la uid generada por Firebase y actualizar el documento con esa uid como idProduct
+    const uid = docRef.id;
+    const inventoryRef = doc(this.firestore, 'inventory', uid);  // Cambio aquí
+    await updateDoc(inventoryRef, 'idProduct', uid);
   }
-  addInventory(inventory: Inventory){
-    const collectionRef = collection(this.firestore, 'inventory')
-    return addDoc(collectionRef, inventory);
+
+  async getAllInventory(): Promise<Inventory[]> {
+    const collectionRef = collection(this.firestore, 'inventory');
+    const snapshot = await getDocs(collectionRef);
+    const inventoryData: Inventory[] = [];
+    snapshot.forEach(doc => {
+      const data = doc.data() as Inventory;
+      inventoryData.push(data);
+    });
+    return inventoryData;
+  }
+
+  async updateInventory(productId: string | undefined, updatedData: Inventory): Promise<void> {
+    if (productId) {
+      const inventoryRef = doc(this.firestore, 'inventory', productId);
+
+      // Verificar la existencia del documento antes de intentar la actualización
+      const docSnapshot = await getDoc(inventoryRef);
+      if (docSnapshot.exists()) {
+        const updatedInventoryData: DocumentData = {
+          ...updatedData,
+          idProduct: updatedData.idProduct || '',
+        };
+
+        // Realizar la actualización
+        await updateDoc(inventoryRef, updatedInventoryData);
+      } else {
+        console.warn('El documento no existe. No se realizará ninguna actualización.');
+      }
+    }
+  }
+
+  async getInventoryByUid(uid: string): Promise<Inventory | undefined> {
+    const collectionRef = collection(this.firestore, 'inventory');
+    const q = query(collectionRef, where('idProduct', '==', uid));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      const docData = querySnapshot.docs[0].data();
+      return docData as Inventory;
+    } else {
+      console.warn('No se encontró el producto con la UID proporcionada');
+      return undefined;
+    }
+  }
+
+  async addSales(sales: Sales): Promise<void> {
+    const collectionRef = collection(this.firestore, 'sales');
+    await addDoc(collectionRef, sales);
   }
 }
